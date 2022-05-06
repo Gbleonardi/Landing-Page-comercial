e9c82cecd75994d2e21987d1e261604c0ab5ad11
Gabriel Leonardi <gabrielleonardi@GabrielleonariDb11.Gabriel>
2022-05-06 07:23:59 -0300

    melhorias na interface

--- JS/main.js
+++ JS/main.js
@@ -6,7 +6,7 @@ function onScroll() {
   }
 }
 
- 
+
 function openMenu() {
   document.body.classList.add('menu-expanded')
 }
@@ -14,3 +14,18 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+  #home, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card
+  #about, 
+  #about header, 
+  #about .content`)
