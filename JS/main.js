2a5509638c4ebb4feab993e6ce2301d4b4b58188
Gabriel Leonardi <gabrielleonardi@GabrielleonariDb11.Gabriel>
2022-05-10 23:05:27 -0300

    commit final

--- JS/main.js
+++ JS/main.js
@@ -6,7 +6,43 @@ function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnscroll()
 
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+
 }
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  // verificar se a base está abaixo da linha alvo
+
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+
+    }
+
+}
+
+
 
 function showNavOnScroll() {
   if (scrollY > 0) {
@@ -16,6 +52,7 @@ function showNavOnScroll() {
   }
 
 }
+
 
 
 function showBackToTopButtonOnscroll() {
@@ -49,5 +86,6 @@ ScrollReveal({
   #about, 
   #about header, 
   #about .content`)
+
 
 
